a2151549fa9de069041e288e288df1b5cc2d261b
Sarfraz Ahmed <sarfraznawaz2005@gmail.com>
2026-04-17 12:07:39 +0500

    chore: release v0.0.13

--- electrobun.config.ts
+++ electrobun.config.ts
@@ -4,7 +4,7 @@ export default {
 	app: {
 		name: "AutoDesk",
 		identifier: "com.sarfrazai.autodesk",
-		version: "0.0.12",
+		version: "0.0.13",
 	},
 	runtime: {
 		// Keep Bun alive when the last window closes so we can recreate it
